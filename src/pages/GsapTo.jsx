8bf90b0d8a6d5516430ede48053c9c58d4045b6a
Pratham943 <phoneixwasfound69@gmail.com>
2025-07-29 21:59:25 +0545

    timeline

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,6 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
-
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 250,
+      repeat: -3,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: "elastic",
+    });
+  }, []);
   return (
     <main>
       <h1>GsapTo</h1>
